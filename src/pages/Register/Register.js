8299ab8fae95e64af96077c03e6bec0ab4955ac0
marycala <marycala87@gmail.com>
2024-10-30 07:01:48 +0100

    solucionado peticion register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -62,11 +62,7 @@ const submitRegister = async (name, email, password, form) => {
   }
 
   try {
-    const res = await apiFetch(
-      `${API_URL}/api/v1/users/register`,
-      'POST',
-      userData
-    )
+    const res = await apiFetch(`/api/v1/users/register`, 'POST', userData)
 
     if (!res.ok) {
       const errorMessage =
