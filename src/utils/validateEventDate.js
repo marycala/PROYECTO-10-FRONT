65931965b246392f099a426470528a888fe0ae6a
marycala <marycala87@gmail.com>
2024-11-17 12:52:54 +0100

    elimiar archivo

--- src/utils/validateEventDate.js
+++
@@ -1,8 +0,0 @@
-export function validateEventDate(date) {
-  const eventDate = new Date(date)
-  const currentDate = new Date()
-  if (eventDate <= currentDate) {
-    return 'The event date must be in the future'
-  }
-  return null
-}
